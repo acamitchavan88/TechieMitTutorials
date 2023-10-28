3c77a05e72aa7b81226b60ae247d32a754f1c163
Amit Chavan <acamitchavan5@gmail.com>
2023-10-28 14:56:07 +0530

    RunTime Exception Demo

---
+++ RunTimeExceptionDemo/src/CustomException.java
@@ -0,0 +1,15 @@
+
+public class CustomException  extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String message;
+	
+	public CustomException(String message)
+	{
+		super(message);
+		
+	}
+
+}
