08927eba18bd3dae28ac4311eeb0365433e323be
Amit Chavan <acamitchavan5@gmail.com>
2024-02-11 08:35:21 +0530

    Code changes to add Java8 code

--- MultiThreadingSamples/src/InterCommunicationDemo.java
+++ MultiThreadingSamples/src/InterCommunicationDemo.java
@@ -4,8 +4,10 @@
  */
 class WorkerThread extends Thread { 
 	int prodofNNos = 1;
+	/* Old Java way of coding
+	int prodofNNos = 1;
 	public void run() {
-		System.out.println("In run method of worker thread ");
+		System.out.println("In run metho d of worker thread ");
 		synchronized (this) {
 			for (int i = 1; i <= 4; i++)
 				prodofNNos = prodofNNos * i;
@@ -13,12 +15,27 @@ class WorkerThread extends Thread {
 			// Notify the waiting thread 4 *3 *2 *1 =24
 			this.notify();
 		}
-	}
+	} */
+	//Using Java 8 below code
+	Thread threadDemo=new Thread(()->{
+		
+		System.out.println("In run method of worker thread ");
+		synchronized (this) {
+			for (int i = 1; i <= 9; i++)
+				prodofNNos = prodofNNos * i;
+			System.out.println("N numbers product is calculated");
+			// Notify the waiting thread 4 *3 *2 *1 =24
+			this.notify();
+		}
+		
+	});
+	
 }   
 public class InterCommunicationDemo {
 	public static void main(String p[]) throws InterruptedException {
+	
 		WorkerThread workThreadInstance = new WorkerThread();
-		workThreadInstance.start();
+		workThreadInstance.threadDemo.start();
 		// Give call for product of n numbers
 		synchronized (workThreadInstance) {// Wait will release main thread	// monitor /lock
 			workThreadInstance.wait();
