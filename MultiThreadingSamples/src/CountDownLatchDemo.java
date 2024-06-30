9850f58a857d205812bb010a7898dd15cd361a20
Amit Chavan <acamitchavan5@gmail.com>
2024-06-30 19:24:23 +0530

    CountDownLatch demo in Java

---
+++ MultiThreadingSamples/src/CountDownLatchDemo.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/*
+  Author #TechieMit
+  Program to display demo of CountDownLatch in Java
+*/
+class Worker implements Runnable {
+	CountDownLatch countDownLatch;
+
+	public Worker(CountDownLatch countDownLatch) {
+		this.countDownLatch = countDownLatch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			showCountLatchDemo();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showCountLatchDemo() throws InterruptedException {
+
+		for (int i = 0; i < 10; i++) {
+			countDownLatch.countDown();
+
+			System.out.println("The current countDownLatch count ::"
+					+ countDownLatch.getCount());
+			TimeUnit.SECONDS.sleep(2);
+		}
+	}
+}
+
+public class CountDownLatchDemo {
+	public static void main(String[] s) throws InterruptedException {
+		int numberOfThreads = 3;
+
+		CountDownLatch latch = new CountDownLatch(numberOfThreads * 10);
+
+		for (int i = 0; i < numberOfThreads; i++) {
+			Worker worker = new Worker(latch);
+			Thread threadDemo = new Thread(worker);
+			threadDemo.start();
+		}
+		latch.await();
+		System.out
+				.println("CountDownLatch demo finished all threads executed in main thread");
+
+	}
+
+}
